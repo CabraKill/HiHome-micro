3b0a66bba5e20c4ff9d3001274890309b3d519ed
CabraKill <raphaeldesouza@outlook.com>
2021-11-26 16:25:36 -0300

    feat: write flight time to firestore

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ MicroControllers files with examples
 # Drivers
 * https://github.com/nodemcu/nodemcu-devkit/blob/master/Drivers/CH341SER_WINDOWS.zip
 * https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers#software
+* https://github.com/esp8266/Arduino/issues/3551
--- esp01-continuous-firestore/esp01-continuous-firestore.ino
+++ esp01-continuous-firestore/esp01-continuous-firestore.ino
@@ -8,21 +8,17 @@
 #include "src/read_data/read_data.h"
 #include "src/calcFlightTime/calcFligthTime.h"
 #include "src/credentials/credentials.h"
+#include "src/writeData/writeFlightTimeOnFirestore.h"
 
 WiFiClient wifiClient;
 
 #define RELAY 0
 
-const char *ssid = "Ric Net";
-const char *password = "jrspcmcs03";
 //const char *ssid = "BRONQUINHA";
 //const char *password = "jrspcmcs03";
 
 const int port = 443;
-
-String ip = "";
-String mac = "60:01:94:21:E7:FA";
-const char *host = "home-dbb7e.rj.r.appspot.com";
+const String host = "home-dbb7e.rj.r.appspot.com";
 
 const String loginHost = "identitytoolkit.googleapis.com";
 const String path = "/v1/accounts:signInWithPassword?key=AIzaSyAIdWnqjoG0uo3Z2CYYpB0IHig1CqtLpKA";
@@ -30,16 +26,13 @@ const String path = "/v1/accounts:signInWithPassword?key=AIzaSyAIdWnqjoG0uo3Z2CY
 //SHA-1 Fingerprint
 const char fingerprintIdentityToolKit[] PROGMEM = "68 DB 23 67 82 7A 3D 3A 3D 68 E1 7B DD 8E 49 36 FB 46 8B B8";
 const char fingerprintFirestore[] PROGMEM = "46 65 29 45 00 73 43 B9 68 B2 B9 47 9D 4B FD 21 19 C8 EF CF";
+const String pathFlightTime = "/v1/projects/home-dbb7e/databases/(default)/documents/unities/rft43A10RZ4LOmMQ6gry/sections/Y2OksEM7ErCqr2jx8UQJ/devices/xC8UGLSYT8z2pxwKaAeY/flightTime";
 
 const int delayBetweenReads = 230;
 
-bool ok = false;
-
 String body = "";
 String token = "";
-int statusCode = -1;
-
-bool done = false;
+String currentToken = "";
 
 WiFiClientSecure httpsClient;
 
@@ -64,23 +57,18 @@ void setup()
 
   Serial.print("\n\n");
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.println(wifiSSID);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifiSSID, wifiPassword);
 
   checkWifi();
 
   // Print the IP address
-  Serial.print("Use this URL to connect: ");
+  Serial.print("Ip address: ");
   Serial.print(WiFi.localIP());
-  Serial.println("/");
-  ip = WiFi.localIP().toString();
-  //  mac = WiFi.macAddress();
-  Serial.println(ip);
 }
 
-String currentToken = "";
 void loop()
 {
   unsigned long now = millis();
@@ -94,16 +82,21 @@ void loop()
     currentToken = "";
     return;
   }
-  if(value == "on"){
+  if (value == "on")
+  {
     digitalWrite(RELAY, LOW);
-  } else{
+  }
+  else
+  {
     digitalWrite(RELAY, HIGH);
   }
+  unsigned long flight = getFlightTime(now);
+
+  writeFlightTimeOnFirestore("firestore.googleapis.com", pathFlightTime, String(flight), currentToken, fingerprintFirestore);
   Serial.println("value: " + value);
 
-
-  unsigned long flight = getFlightTime(now);
-  if(flight > delayBetweenReads){
+  if (flight > delayBetweenReads)
+  {
     return;
   }
   delay(delayBetweenReads - flight);
